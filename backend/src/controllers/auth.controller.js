53ff6f3e1a4f5084c4f3a5ab99bf8532aba7757f
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-12 00:53:03 +0530

    made add students and join class

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,41 +1,58 @@
 import User from '../models/user.model.js';
 import axios from 'axios';
-import {oauth2Client} from '../utils/googleConfig.js';
+import {OAuth2Client} from 'google-auth-library';
+// import {oauth2Client} from '../utils/googleConfig.js';
 import jwt from 'jsonwebtoken';
 
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
 const googleLogin = async (req, res) => {
+    const client = new OAuth2Client(CLIENT_ID);
+    console.log("Hello im in google login func")
     try {
-        const {code} = req.query;
-        const googleRes = await oauth2Client.getToken(code);
-        oauth2Client.setCredentials(googleRes.tokens);
+        const token = req.headers.authorization?.split(' ')[1];  // Extract the token from the Authorization header        const googleRes = await oauth2Client.getToken(code);
+        // oauth2Client.setCredentials();
+        
+        // const userRes = await axios.get(`https://www.googleapis.com/oauth2/v1/
+        //     userinfo?alt=json&access_token=${code}` 
+        // )
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+          });
 
-        const userRes = await axios.get(`https://www.googleapis.com/oauth2/v1/
-            userinfo?alt=json&access_token=${googleRes.tokens.access_token}` 
-        )
-        const {email, name} = userRes.data;
+        const payload = ticket.getPayload();  // Get the user information
+        console.log('User verified:', payload);
+
+        const email = payload.email;
+        const name = payload.name;
         let user = await User.findOne({email});
-
+        console.log(email,name);
+        
         if(!user){
+            console.log("Creating user");
             user = await User.create({
-                email,
-                name
+                email: email,
+                display_name: name
             })
         }
+        console.log(user);
         const {_id} = user;
-        const token = jwt.sign({_id, email},
+        const jwtToken = jwt.sign({_id, email},
             process.env.JWT_SECRET,
             {expiresIn: process.env.JWT_TIMEOUT}
         );
-
         return res.status(200).json({
             success: true,
-            token,
+            jwtToken,
             user
         })
     } catch (error) {
         res.status(500).json({
             success: false,
-            message:"Internal server error"
+            message: error.message
         })
         
     }
