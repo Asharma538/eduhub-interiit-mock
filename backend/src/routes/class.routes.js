96e027428f2e68b8afb89783b54bc52aa64519bb
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-12 04:27:52 +0530

    minor debugging

--- backend/src/routes/class.routes.js
+++ backend/src/routes/class.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { joinClass, createClass, getClasses, getClassData} from "../controllers/class.controller.js";
-import { authenticateJWT } from "../middlewares/jwt.middleware.js";
+import { joinClassroom, createClassroom, getClasses, getClassData} from "../controllers/class.controller.js";
+import authenticateJWT  from "../middlewares/jwt.middleware.js";
 
 const router= Router();
 
-router.post('/join', authenticateJWT ,joinClass);
-router.post('/create', authenticateJWT,createClass);
+router.post('/join', authenticateJWT ,joinClassroom);
+router.post('/create', authenticateJWT,createClassroom);
 router.get('/classes', authenticateJWT, getClasses);
 router.get('/classes/:classId',authenticateJWT, getClassData)
 
