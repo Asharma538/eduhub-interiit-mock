195da81a9a3310148e973a47c9a1dffefcb07a53
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-10-12 00:47:22 +0530

    create classroom and getMember controller done

--- backend/src/controllers/class.controller.js
+++ backend/src/controllers/class.controller.js
@@ -1,8 +1,42 @@
 import { asyncHandler } from "../utils/asynchandler";
+import Classroom from "../models/classroom.model";
+import User from "../models/user.model";
 
 export const joinClassroom = asyncHandler(async(req, res) => {});
 
-export const createClassroom = asyncHandler(async(req, res) => {});
+export const createClassroom = asyncHandler(async(req, res) => {
+    try {
+        const userId = req.user.id;
+        const {name, invitation_code, details} = req.body;
+        const newClassroom = new Classroom.create({
+            name,
+            invitation_code,
+            details,
+            teachers: [userId]
+        });
+        
+        const savedClassroom = await newClassroom.save();
+        const user = await User.findById(userId);
+        
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        user.classes.push(savedClassroom._id);
+        await user.save();
+        return res.status(201).json({
+            message: "Classroom created successfully",
+            classroom: savedClassroom
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error", error });
+    }
+});
 
 export const getClasses = asyncHandler(async(req, res) => {});
 
--- backend/src/controllers/member.controller.js
+++ backend/src/controllers/member.controller.js
@@ -1,8 +1,41 @@
 import { asyncHandler } from "../utils/asynchandler";
+import Classroom from "../models/classroom.model";
 
 export const addStudent = asyncHandler(async(req, res) => {});
 
 export const addTeacher = asyncHandler(async(req, res) => {});
 
-export const getMembers = asyncHandler(async(req, res) => {});
+export const getMembers = asyncHandler(async(req, res) => {
+    try {
+    const { classroomId } = req.params;
+    const classroom = await Classroom.findById(classroomId)
+        .populate('teachers', 'display_name email') 
+        .populate('students', 'display_name email');
+    if (!classroom) {
+        return res.status(404).json({
+            success: false,
+            message: "Classroom not found"
+        })
+    }
+
+    const teachers = classroom.teachers.map(teacher => ({
+        email: teacher.email,
+        display_name: teacher.display_name
+    }));
+
+    const students = classroom.students.map(student => ({
+        email: student.email,
+        display_name: student.display_name
+    }));
+
+    return res.status(200).json({
+        success: true,
+        teachers,
+        students
+    });
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error", error });
+}}
+);
 
