3095a6266fc9dc678126d461a8f42bc4c347d376
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-11 19:06:06 +0530

    class and member routes and controller files made and minor debugging

---
+++ backend/src/controllers/member.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asynchandler";
+
+export const addStudent = asyncHandler(async(req, res) => {});
+
+export const addTeacher = asyncHandler(async(req, res) => {});
+
+export const getMembers = asyncHandler(async(req, res) => {});
+
---
+++ backend/src/routes/member.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { addStudent, addTeacher, getMembers} from "../controllers/member.controller.js";
+import { authenticateJWT } from "../middlewares/jwt.middleware.js";
+
+const router= Router();
+
+router.post('/classes/:classId/add/student', authenticateJWT ,addStudent);
+router.post('/classes/:classId/add/teacher', authenticateJWT,addTeacher);
+router.get('/classes/:classId/members', authenticateJWT, getMembers);
+
+
+export default router;
