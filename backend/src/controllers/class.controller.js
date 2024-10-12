f9064ecbef903ed48d1ae5daafc1158140e54700
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-13 00:50:56 +0530

    getClass and postAssignment made

--- backend/src/controllers/class.controller.js
+++ backend/src/controllers/class.controller.js
@@ -1,4 +1,7 @@
+import Assignment from "../models/assignment.model.js";
+import Announcement from "../models/anouncement.model.js";
 import Classroom from "../models/classroom.model.js";
+import Comment from "../models/comment.model.js";
 import User from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -114,7 +117,68 @@ export const getClasses = asyncHandler(async(req, res) => {
     res.status(200).json(new ApiResponse(200,classesData))
 });
 
-export const getClassData= asyncHandler(async(req, res) => {});
+export const getClassData= asyncHandler(async(req, res) => {
+    const classId = req.params.classId;
+    const userId = req.user._id;
+
+    const classroom = await Classroom.findById(classId)
+    .populate({
+        path:"announcements",
+        populate: [
+            {
+                path: "user_id",
+                select: "email display_name"
+            },
+            {
+                path:"comments",
+                populate:{
+                    path:"user_id",
+                    select:"email display_name"
+                },
+                select:"content"
+            }
+        ]
+    });
+
+    if(!classroom)
+    {
+        throw new ApiError(404,"Classroom not found.");
+    }
+
+    const user= await User.findById(userId)
+    if(!user)
+    {
+        throw new ApiError(404,"User not found");
+    }
+
+    if(!classroom.teachers.includes(userId) && !classroom.teachers.includes(userId))
+    {
+        throw new ApiError(403, "You are not a memeber of this class")
+    }
+
+    const classroomData={
+        name: classroom.name,
+        details:classroom.details,
+    }
+
+    const announcements= classroom.announcements.map(announcement =>({
+        content :announcement.content,
+        file_url: announcement.file_url,
+        user_id: {
+            display_name: announcement.user_id.display_name,
+            email: announcement.user_id.email
+        },
+        comments: announcement.comments.map(comment => ({
+            content : comment.content,
+            user_id: {
+                display_name: announcement.user_id.display_name,
+                email: announcement.user_id.email
+            }
+        }))
+    }));
+    console.log(announcements)
+    res.status(200).json(new ApiResponse(200,{classroom: classroomData, announcements},"Class data retrieved successfully"));
+});
 
 // Controller to get classwork of a specific classroom
 export const getClassWork = asyncHandler(async(req,res)=>{
@@ -160,42 +224,42 @@ export const getClassWork = asyncHandler(async(req,res)=>{
     }
 });
 
-export const postAnnouncement=asyncHandler(async(req,res)=>{
-    try {
-        // get the classroom id from the frontend
-        const classroomId = req.params.classId;
-        // Now get the classroom with the given id
-        const classroom = await Classroom.findById(classroomId);
+export const postAnnouncement=asyncHandler(async(req,res)=>{});
 
-        if(!classroom){
-            return res.status(404).json({message: "Class not found"});
-        }
-        
-        // get the user id from the request
-        const userId = req.user._id;
+export const postAssignment=asyncHandler(async(req,res)=>{
+    const userId = req.user._id;
+    const {title, description, file_url ,deadline}=req.body;
+    const classId = req.params.classId
 
-        // get the title and content of the announcement from the frontend
-        const {title, content} = req.body;
-
-        // create a new announcement object using the defined schema
-        const announcement = new Announcement({
-            title,
-            content,
-            user_id: userId,
-            classroom_id: classroomId
-        });
-        await announcement.save();
-
-        classroom.announcements.push(announcement);
-
-        await classroom.save();
-        
-        //send the status
-        res.status(200).json({message: "Your announcement has been posted successfully", announcement});
-
-    }catch (error) {
-        // Step 8: Handle any errors
-        res.status(500).json({ message: 'Server error', error });
+    const user = await User.findById(userId)
+    if(!user)
+    {
+        throw new ApiError(404,"User not found")
     }
+
+    const classroom = await Classroom.findById(classId)
+    if(!classroom)
+    {
+        throw new ApiError(404,"Classroom not found")
+    }
+
+    if(!classroom.teachers.includes(userId) && !classroom.teachers.includes(userId))
+    {
+        throw new ApiError(403, "You are not a memeber of this class")
+    }
+
+    const newAssignment = new Assignment({
+        title,
+        description,
+        file_url,
+        deadline,
+        user_id: userId
+    })
+
+    const savedAssignment =  await newAssignment.save();
+
+    const savedClassroom= await classroom.assignments.push(savedAssignment._id)
+    await classroom.save()
+
+    res.status(200).json(200, {savedClassroom},"Announcemented posted successfully")
 });
-export const postAssignment=asyncHandler(async(req,res)=>{});
