cb82525393a67ed283cb8a0306c4092fff12e7f5
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-14 00:46:24 +0530

    completed post announcement

--- backend/src/controllers/class.controller.js
+++ backend/src/controllers/class.controller.js
@@ -1,10 +1,8 @@
 import Classroom from "../models/classroom.model.js";
-import Comment from "../models/comment.model.js";
 import User from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asynchandler.js";
-import  Assignment  from "../models/assignment.model.js";
 import Announcement from "../models/anouncement.model.js";
 // import Announcement from "../models/announcement.model.js";
 
@@ -116,7 +114,40 @@ export const getClassWork = asyncHandler(async(req,res)=>{
 });
 
 export const postAnnouncement=asyncHandler(async(req,res)=>{
+    try {
+        const {content,file_url} = req.body; //extract the text from the request body 
+        
+        const classroomId = req.params.classId; // get the class id from the request param url
+        const userId = req.user._id;
+        console.log(userId);
+        
+        // check if the class exists
+        const classroom = await Classroom.findById(classroomId);
+        if(!classroom){
+            return res.status(404).json({message: "class not found"});
+        }
 
+        // create a new announcement instance
+        const newAnnouncement = new Announcement({
+            file_url:file_url,
+            content: content,
+            user_id: userId
+        });
+
+        console.log(newAnnouncement);
+
+        await newAnnouncement.save();
+        
+        classroom.announcements.push(newAnnouncement._id);
+
+        await classroom.save(); // Save the updated classroom document
+
+        console.log(classroom);
+        res.status(201).json({ message: 'Announcement posted successfully'});
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server error aa rahi', error });
+    }
 });
 
 export const deleteClass=asyncHandler(async(req,res)=>{
