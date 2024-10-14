dd4ba7b1ae14de13de0e28938b7d0f80b66cb4c3
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-14 22:40:30 +0530

    sending invite code in resp of the user is teacher in get class

--- backend/src/controllers/class.controller.js
+++ backend/src/controllers/class.controller.js
@@ -7,108 +7,119 @@ import Announcement from "../models/anouncement.model.js";
 import axios from 'axios';
 // import Announcement from "../models/announcement.model.js";
 
-export const getClassData= asyncHandler(async(req, res) => {
+export const getClassData = asyncHandler(async (req, res) => {
     const classId = req.params.classId;
     const userId = req.user._id;
 
     const classroom = await Classroom.findById(classId)
-    .populate({
-        path:"announcements",
-        populate: [
-            {
-                path: "user_id",
-                select: "email display_name"
-            },
-            {
-                path:"comments",
-                populate:{
-                    path:"user_id",
-                    select:"email display_name"
-                },
-                select:"content"
-            }
-        ]
-    });
-
-    if(!classroom)
-    {
-        throw new ApiError(404,"Classroom not found.");
-    }
-
-    const user= await User.findById(userId)
-    if(!user)
-    {
-        throw new ApiError(404,"User not found");
-    }
-
-    if(!classroom.teachers.includes(userId) && !classroom.teachers.includes(userId))
-    {
-        throw new ApiError(403, "You are not a memeber of this class")
-    }
-
-    const classroomData={
-        name: classroom.name,
-        details:classroom.details,
-    }
-
-    const announcements= classroom.announcements.map(announcement =>({
-        id:announcement._id,
-        content :announcement.content,
-        file_url: announcement.file_url,
-        user_id: {
-            display_name: announcement.user_id.display_name,
-            email: announcement.user_id.email
-        },
-        comments: announcement.comments.map(comment => ({
-            content : comment.content,
-            user_id: {
-                display_name: announcement.user_id.display_name,
-                email: announcement.user_id.email
-            }
-        }))
-    }));
-    console.log(announcements)
-    res.status(200).json(new ApiResponse(200,{classroom: classroomData, announcements},"Class data retrieved successfully"));
-});
-
-// Controller to get classwork of a specific classroom
-export const getClassWork = asyncHandler(async(req,res)=>{
-    try {
-        // get the classroom id from the frontend
-        const classroomId = req.params.classId;
-
-        // Now get the classroom with the given id
-        const classroom = await Classroom.findById(classroomId)
-        
-
-        if(!classroom){
-            return res.status(404).json({message: "Class not found"});
-        }
-
-        // we need to fetch classwork associated with the classroom
-        const classwork = await Classroom.findById(classroomId)
         .populate({
             path: "announcements",
             populate: [
                 {
                     path: "user_id",
                     select: "email display_name"
-                }
-            ]
-        })
-        .populate({
-            path: "assignments",
-            populate: [
+                },
                 {
-                    path: "user_id",
-                    select: "email display_name"
+                    path: "comments",
+                    populate: {
+                        path: "user_id",
+                        select: "email display_name"
+                    },
+                    select: "content"
                 }
             ]
-        }).select("assignments announcements");
+        });
+
+    if (!classroom) {
+        throw new ApiError(404, "Classroom not found.");
+    }
+
+    const user = await User.findById(userId)
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isStudent = classroom.students.includes(userId);
+    const isTeacher = classroom.teachers.includes(userId);
+    if (!isStudent && !isTeacher) {
+        throw new ApiError(403, "You are not a memeber of this class")
+    }
+
+    let classroomData;
+
+    if (isStudent) {
+        classroomData = {
+            name: classroom.name,
+            details: classroom.details,
+        }
+    }
+
+    if (isTeacher) {
+        classroomData = {
+            invitation_code: classroom.invitation_code,
+            name: classroom.name,
+            details: classroom.details,
+        }
+    }
+
+    const announcements = classroom.announcements.map(announcement => ({
+        id: announcement._id,
+        content: announcement.content,
+        file_url: announcement.file_url,
+        user_id: {
+            display_name: announcement.user_id.display_name,
+            email: announcement.user_id.email
+        },
+        comments: announcement.comments.map(comment => ({
+            content: comment.content,
+            user_id: {
+                display_name: announcement.user_id.display_name,
+                email: announcement.user_id.email
+            }
+        }))
+    }));
+    // console.log(announcements)
+    res.status(200).json(new ApiResponse(200, { classroom: classroomData, announcements }, "Class data retrieved successfully"));
+});
+
+// Controller to get classwork of a specific classroom
+export const getClassWork = asyncHandler(async (req, res) => {
+    try {
+        // get the classroom id from the frontend
+        const classroomId = req.params.classId;
+
+        // Now get the classroom with the given id
+        const classroom = await Classroom.findById(classroomId)
+
+
+        if (!classroom) {
+            return res.status(404).json({ message: "Class not found" });
+        }
+
+        // we need to fetch classwork associated with the classroom
+        const classwork = await Classroom.findById(classroomId)
+            .populate({
+                path: "announcements",
+                populate: [
+                    {
+                        path: "user_id",
+                        select: "email display_name"
+                    }
+                ]
+            })
+            .populate({
+                path: "assignments",
+                populate: [
+                    {
+                        path: "user_id",
+                        select: "email display_name"
+                    }
+                ]
+            }).select("assignments announcements");
 
         //return response as classwork as json response
         res.status(200).json(classwork);
-        
+
 
     } catch (error) {
         res.status(500).json({ message: 'Server error ', error });
@@ -157,9 +168,9 @@ export const deleteClass = asyncHandler(async (req, res) => {
     await Promise.all(
         classroom.assignments.map(async (assignmentId) => {
             try {
-                await axios.delete(`http://localhost:8000/classes/${classId}/assignments/${assignmentId}`,{
-                    headers:{
-                        Authorization:token
+                await axios.delete(`http://localhost:8000/classes/${classId}/assignments/${assignmentId}`, {
+                    headers: {
+                        Authorization: token
                     }
                 });
             } catch (error) {
@@ -171,9 +182,9 @@ export const deleteClass = asyncHandler(async (req, res) => {
     await Promise.all(
         classroom.announcements.map(async (announcementId) => {
             try {
-                await axios.delete(`http://localhost:8000/classes/${classId}/announcements/${announcementId}`,{
-                    headers:{
-                        Authorization:token
+                await axios.delete(`http://localhost:8000/classes/${classId}/announcements/${announcementId}`, {
+                    headers: {
+                        Authorization: token
                     }
                 });
             } catch (error) {
