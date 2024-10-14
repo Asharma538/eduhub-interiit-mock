6d7b7c77b55559dec31f8783c863e9164ce8039b
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-14 06:09:09 +0530

    minor changes in router and added submit assignment router

--- backend/src/routes/assignment.routes.js
+++ backend/src/routes/assignment.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { postAssignment ,getAssignment, deleteAssignment} from '../controllers/assignment.controller.js';
+import { postAssignment ,getAssignment, deleteAssignment, submitAssignment} from '../controllers/assignment.controller.js';
 import authenticateJWT from '../middlewares/jwt.middleware.js';
 
-const router = express.Router()
+const router = express.Router({mergeParams:true})
 
-router.get('/postAssignment',authenticateJWT, postAssignment);
+router.post('/',authenticateJWT, postAssignment);
 router.get('/:assignmentId',authenticateJWT,getAssignment);
 router.delete('/:assignmentId',authenticateJWT,deleteAssignment);
+router.post('/:assignmentId',authenticateJWT,submitAssignment);
 
 export default router
