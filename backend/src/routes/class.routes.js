88d1f45d32c23cbbecbf5042cbabbf532a2d3a91
Lakshya Jain <lakshyajainrj19@gmail.com>
2024-10-14 06:08:14 +0530

    removed repetating :classId and moved deleteAnnouncement router

--- backend/src/routes/class.routes.js
+++ backend/src/routes/class.routes.js
@@ -1,20 +1,17 @@
 import { Router } from "express";
-import { getClassData, getClassWork, postAnnouncement, deleteClass, deleteAnnouncement} from "../controllers/class.controller.js";
+import { getClassData, getClassWork, deleteClass} from "../controllers/class.controller.js";
 import authenticateJWT  from "../middlewares/jwt.middleware.js";
 
 const router = Router({ mergeParams: true });
 
 
-router.get('/:classId',authenticateJWT, getClassData);
+router.get('/',authenticateJWT, getClassData);
 
 // route to get the classwork of a class
-router.get('/:classId/classwork', authenticateJWT, getClassWork);
+router.get('/classwork', authenticateJWT, getClassWork);
 
 // route to post an announcement in a class
-router.post('/:classId/announcements/post',authenticateJWT, postAnnouncement);
 
-router.delete('/deleteClass',authenticateJWT, deleteClass);
-
-router.delete('/deleteAnnouncement',authenticateJWT, deleteAnnouncement);
+router.delete('/',authenticateJWT, deleteClass);
 
 export default router;
